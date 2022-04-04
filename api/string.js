9a06a6f1169aac4d3ba10e0035e204dc1a8fdece
Chris Chinchilla <chris@chrischinchilla.com>
2022-04-04 15:50:13 +0200

    Switch to serverless

---
+++ api/string.js
@@ -0,0 +1,22 @@
+const express = require("express");
+var bgjgLexicon = require("bgjg");
+var cors = require('cors');
+
+const app = express();
+const port = process.env.PORT || "8000";
+
+app.use(cors());
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+
+app.listen(port, () => {
+    console.log(`Listening to requests on ${port}`);
+});
+
+app.get("/", (req, res) => {
+    var bgjgText = bgjgLexicon.generateLexicon();
+    res.status(200).json({ message: bgjgText });
+});
